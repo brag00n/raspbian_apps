fde3b19bfb6d11c5949a5b6ce9dbb670d3fa91a2
Shinya Ishikawa <ishikawa@rt-net.jp>
2022-01-12 07:00:05 +0000

    Fix application name

--- int32_publisher/app_info.sh
+++ int32_publisher/app_info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export APP_PACKAGE_NAME=mysecondapp
-export APP_BINARY_NAME=mysecondapp
-export APP_OUTPUT_NAME=mysecondapp
+export APP_PACKAGE_NAME=int32_publisher
+export APP_BINARY_NAME=int32_publisher
+export APP_OUTPUT_NAME=int32_publisher
 export RMW_IMPLEMENTATION=rmw_microxrcedds
